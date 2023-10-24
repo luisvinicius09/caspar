a3fff59b50cc008bbfead19090fdf776e777db3c
Luis V <luisvinicius0906@gmail.com>
2023-10-24 08:20:40 -0300

    Add: simple fetching data example to charges page file

--- src/app/(authorized)/charges/page.tsx
+++ src/app/(authorized)/charges/page.tsx
@@ -1,13 +1,23 @@
+async function getData(): Promise<any> {
+	const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/charges`);
+
+	if (!res.ok) {
+		throw new Error('Failed to fetch data');
+	}
+
+	return res.json();
+}
+
 export default function Charges() {
+	const data = getData();
+
 	return (
 		<>
 			<div className='bg-secondary/70 w-full h-full max-h-[44rem] pb-12 p-8 rounded-xl shadow-lg'>
 				<div className='flex justify-between items-center mb-4'>
 					<div className='w-32 h-8 bg-accent rounded-lg'></div>
 
-					<button className='bg-primary text-white p-2 rounded-lg'>
-						+ Nova cobrança
-					</button>
+					<button className='bg-primary text-white p-2 rounded-lg'>+ Nova cobrança</button>
 				</div>
 
 				<div className=''>
